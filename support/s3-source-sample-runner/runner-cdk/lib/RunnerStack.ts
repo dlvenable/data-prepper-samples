63bf88c4dab80a42c4ea1ea2a7b945df8dea098c
David Venable <dlv@amazon.com>
2022-11-10 19:28:59 -0600

    Minor improvements to the S3 runner CDK stack.

--- support/s3-source-sample-runner/runner-cdk/lib/RunnerStack.ts
+++ support/s3-source-sample-runner/runner-cdk/lib/RunnerStack.ts
@@ -1,8 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import {Construct} from 'constructs';
+import {DockerImageAsset} from 'aws-cdk-lib/aws-ecr-assets';
 import * as path from "path";
-import {Cluster, ContainerImage, FargateService, FargateTaskDefinition} from "aws-cdk-lib/aws-ecs";
+import {AwsLogDriver, Cluster, ContainerImage, FargateService, FargateTaskDefinition} from "aws-cdk-lib/aws-ecs";
+import {LogGroup, RetentionDays} from "aws-cdk-lib/aws-logs";
 
 export class RunnerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,6 +13,9 @@ export class RunnerStack extends cdk.Stack {
     if(targetUrl === undefined) {
       throw new Error('Must specify the targetUrl.')
     }
+
+    const definedTaskCount: string = scope.node.tryGetContext('taskCount');
+    const taskCount = definedTaskCount !== undefined ? parseInt(definedTaskCount) : 2;
 
     const dockerImage = new DockerImageAsset(this, 'SampleRunner', {
       directory: path.join(__dirname, '../..'),
@@ -25,15 +29,22 @@ export class RunnerStack extends cdk.Stack {
       cpu: 256,
     });
 
+    const runnerLogGroup = new LogGroup(this, 'SampleRunnerLogGroup', {
+      retention: RetentionDays.ONE_DAY
+    });
     sampleRunnerTaskDefinition.addContainer('SampleRunner', {
       image: ContainerImage.fromDockerImageAsset(dockerImage),
-      command: [targetUrl]
+      command: [targetUrl],
+      logging: new AwsLogDriver({
+        logGroup: runnerLogGroup,
+        streamPrefix: 's3-source-sample-runner/'
+      })
     });
 
     const sampleRunnerService = new FargateService(this, 'SampleRunnerService', {
       cluster,
       taskDefinition: sampleRunnerTaskDefinition,
-      desiredCount: 10,
+      desiredCount: taskCount,
     });
   }
 }
